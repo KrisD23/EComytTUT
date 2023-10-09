f71eafd159e2181543b10e4495f6b7a88a47fa90
Krishan Dwivedi <krishandwive9@gmail.com>
2023-10-09 21:19:03 +0530

    Intro page completed

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -7,16 +7,66 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            //logo
-            Image.asset('lib/images/Logo.png')
-            //title
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset('lib/images/Logo.png'),
+              ),
 
-            //sub title
+              const SizedBox(
+                height: 40,
+              ),
+              //title
+              const Text('Live For Greatness',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  )), //Text('Live for greatness.'
 
-            //start shopping button
-          ],
+              const SizedBox(
+                height: 40,
+              ),
+              //sub title
+              const Text(
+                "It’s more than a sign — it’s a commitment",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(
+                height: 40,
+              ),
+
+              //start shopping button
+              GestureDetector(
+                // onTap: () => Navigator.push(
+                //     context, MaterialPageRoute(builder: (context) => HomePage(),),),),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: const EdgeInsets.all(25),
+                  child: const Center(
+                    child: Text('Shop now',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        )),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
