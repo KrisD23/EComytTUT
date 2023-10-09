d335c4ffef233a8d7d07f7a57b3ef4580d4a5b03
Krishan Dwivedi <krishandwive9@gmail.com>
2023-10-09 18:59:16 +0530

    Added the logo

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            //logo
+            Image.asset('lib/images/Logo.png')
+            //title
+
+            //sub title
+
+            //start shopping button
+          ],
+        ),
+      ),
+    );
+  }
+}
